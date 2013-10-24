6a6eae8aab47a70a9509957c60262263ffd92e5b
i-Hun <i.hun@ya.ru>
2013-10-24 18:03:33 +0700

    Изменения главной страницы
    Три раздела: Новости, События, Объявления

--- wp-content/themes/socio3/main-page.php
+++ wp-content/themes/socio3/main-page.php
@@ -6,48 +6,65 @@ get_header(); ?>
 
 <!-- Row for main content area -->
 	<div class="large-9 columns news">
-		<h2 class="titles">Новости</h2>
-		<hr>
-		<?php
-			// $postcounter = 1;
-			$recent = new WP_Query(array( 'showposts' => '10', 'cat' => '-9' ));
-			while($recent->have_posts()) : $recent->the_post(); $postcounter++;
-			global $more;
-			$more = 0;
-		?>
-		<?php if (($postcounter - 1) % 2 == 0) : ?><?php echo "<div class='row'>"; ?>
+		<div class="section-container tabs" data-section="tabs">
+			<section class="active">
+				<p class="title " data-section-title><a href="#news">Новости</a></p>
+				<div class="content" data-section-content>
+					<p>
+						<?php
+							// $postcounter = 1;
+							$recent = new WP_Query(array( 'showposts' => '10', 'cat' => '-9' ));
+							while($recent->have_posts()) : $recent->the_post(); $postcounter++;
+							global $more;
+							$more = 0;
+						?>
+						<?php if (($postcounter - 1) % 2 == 0) : ?><?php echo "<div class='row'>"; ?>
 
-		<?php endif; ?>
-		<article class="nice_block large-6 columns<?php $selected = $cfs->get('selected_post');
-			if ($selected) {
-			 	echo 'selected';
-			 } 
-			?>">
+						<?php endif; ?>
+						<article class="nice_block large-6 columns<?php $selected = $cfs->get('selected_post');
+							if ($selected) {
+							 	echo 'selected';
+							 } 
+							?>">
 
-			<a href="<?php the_permalink() ?>">
-				<div class="pad"><h3><?php the_title(); ?></h3></div>
+							<a href="<?php the_permalink() ?>">
+								<div class="pad"><h3><?php the_title(); ?></h3></div>
 
-				<?php
-					$post_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(600,600));
-				
-				if (function_exists ("has_post_thumbnail") && has_post_thumbnail () ) {
-					echo '<div class="post_thumbnail" style="background-image: url(' . $post_thumbnail[0] . ')"></div>';	
-				}
-				?>
-				</a>
+								<?php
+									$post_thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), array(600,600));
+								
+								if (function_exists ("has_post_thumbnail") && has_post_thumbnail () ) {
+									echo '<div class="post_thumbnail" style="background-image: url(' . $post_thumbnail[0] . ')"></div>';	
+								}
+								?>
+								</a>
 
-				<div class="pad announce_text hyphenate"><?php the_content(''); ?></div>		
-		</article>
-		<?php if ($postcounter % 2 == 0) : ?><?php echo "</div>"; ?>
+								<div class="pad announce_text hyphenate"><?php the_content(''); ?></div>		
+						</article>
+						<?php if ($postcounter % 2 == 0) : ?><?php echo "</div>"; ?>
 
-		<?php endif; ?>
-		<?php endwhile; ?>
-	<div class="row">
-		<div class="large-4 large-centered columns"><a href="/?cat=4" class="button expand more_from_cat">Остальные записи</a></div>
-	</div>
+						<?php endif; ?>
+						<?php endwhile; ?>
+					</p>
+				</div>
+				<a href="/?cat=4" class="more_from_cat">Остальные записи</a>
+
+			</section>
+			<section>
+				<p class="title" data-section-title><a href="#events">События</a></p>
+				<div class="content" data-section-content>
+					<p>Content of section 2.</p>
+					<div class="row">
+						<div class="large-4 large-centered columns"><a href="/?cat=4" class="button expand more_from_cat">Остальные записи</a></div>
+					</div>
+				</div>
+			</section>
+		</div>
+
 	</div>
 	<div class="large-3 columns events">
-		<?php echo events_sidebar(); ?>
+		<h2 class="cat-title">Объявления</h2>
+
 	</div>
 
 
